cbd93101c1c4f6de7c30c4021ab42116d5f721ea
ibrahim11elian <ibrahim11elian@gmail.com>
2024-03-25 23:55:04 +0200

    comment the condition that make the input for code appear in each page

--- src/components/RouteOutlet/RouteOutlet.jsx
+++ src/components/RouteOutlet/RouteOutlet.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import DashboredHeader from '../DashboredHeader/DashboredHeader';
-import { Outlet, useLocation } from 'react-router-dom';
-import FixedRoute from '../FixedRoute';
+import React from "react";
+import DashboredHeader from "../DashboredHeader/DashboredHeader";
+import { Outlet, useLocation } from "react-router-dom";
+import FixedRoute from "../FixedRoute";
 
 export default function RouteOutlet({ title = "", route = "" }) {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
   return (
-    <section className='grow'>
+    <section className="grow">
       <DashboredHeader />
-      <h1 className='font-semibold text-3xl px-10 my-10 text-fuchsia-800'>{title}</h1>
-      {pathname.endsWith(route) ? <FixedRoute route={route} /> : <Outlet />}
+      <h1 className="font-semibold text-3xl px-10 my-10 text-fuchsia-800">
+        {title}
+      </h1>
+      <Outlet />
+      {/* {pathname.endsWith(route)  ? <FixedRoute route={route} /> : <Outlet />} */}
     </section>
-
-  )
+  );
 }
