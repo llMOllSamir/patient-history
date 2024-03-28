5d56f007b1c9413ad75b5e452143ffe907a7eef5
ibrahim11elian <ibrahim11elian@gmail.com>
2024-03-28 08:01:31 +0200

    create Ovarian Cancer slice and added it to store

---
+++ src/store/slices/ovarianSlice.js
@@ -0,0 +1,104 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import Http from "../../fetchingRequest";
+import notify from "../../utilities/alert-toastify";
+
+const initialState = {
+  loading: false,
+  ovarian: {
+    breast_cancer_history: null,
+    relatives_with_ovarian_cancer: null,
+    gene_mutation_or_lynch_syndrome: null,
+    tvs_result: null,
+    tvs_comment: null,
+    "ca-125_result": null,
+    "ca-125_comment": null,
+    recommendations: null,
+  },
+  error: null,
+};
+
+// Get ovarian History data
+export const getOvarian = createAsyncThunk(
+  "ovarian/getOvarianAsync",
+  async ({ id }, { rejectWithValue }) => {
+    try {
+      const response = await Http().get(`/ovarian/${id}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateOvarian = createAsyncThunk(
+  "ovarian/updateOvarianAsync",
+  async ({ id, data }, { rejectWithValue }) => {
+    try {
+      const response = await Http().put(`/ovarian/${id}`, data);
+      if (response.status === 200) {
+        notify("تم تحديث الفحص بنجاح", "success");
+      }
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+// Add ovarian data
+export const addOvarian = createAsyncThunk(
+  "ovarian/addOvarianAsync",
+  async ({ data }, { rejectWithValue }) => {
+    try {
+      const response = await Http().post(`/ovarian/`, data);
+      if (response.status === 201) {
+        notify("تم إضافة الفحص بنجاح", "success");
+      }
+      return response.data;
+    } catch (error) {
+      notify("حدث خطأ أثناء تحديث الفحص", "error");
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const ovarianSlice = createSlice({
+  name: "ovarian",
+  initialState,
+  reducers: {
+    clearOvarianData: (state) => {
+      Object.assign(state, initialState);
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addOvarian.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(addOvarian.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.error = null;
+      state.ovarian = payload.examination;
+    });
+    builder.addCase(addOvarian.rejected, (state) => {
+      state.loading = false;
+      state.error = true;
+    });
+    builder.addCase(getOvarian.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getOvarian.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.error = null;
+      state.ovarian = payload;
+    });
+    builder.addCase(getOvarian.rejected, (state) => {
+      state.loading = false;
+      state.error = true;
+    });
+  },
+});
+
+export const { clearOvarianData } = ovarianSlice.actions;
+export default ovarianSlice.reducer;
