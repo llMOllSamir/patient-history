2b928662a744b0c83a4afcd8517b6cc058b03608
ibrahim11elian <ibrahim11elian@gmail.com>
2024-03-28 04:07:32 +0200

    add PatientPre-eclampsiaHistory UI and logic

--- src/components/PatientPreEclampsia/PatientPreEclampsia.jsx
+++ src/components/PatientPreEclampsia/PatientPreEclampsia.jsx
@@ -1,8 +1,122 @@
-import React from 'react'
-import styles from "./PatientPreEclampsia.module.css";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import LoadingPatient from "../../LoadingPatient";
+import { FiDownload } from "react-icons/fi";
+import { MdAdd, MdEdit } from "react-icons/md";
+import { setPatientId } from "../../store/slices/patientSlice";
+import { getPreEclampsia } from "../../store/slices/preEclampsiaSlice";
 
 export default function PatientPreEclampsia() {
-  return (
-    <div>PatientPreEclampsia</div>
-  )
+  const { id } = useParams();
+  const dispatech = useDispatch();
+  const { preEclampsia, loading, error } = useSelector(
+    (state) => state.preEclampsia
+  );
+
+  useEffect(() => {
+    dispatech(getPreEclampsia({ id: Number(id) })).then((res) => {
+      if (res.payload.id) {
+        dispatech(setPatientId({ id: Number(res.payload.patient_id) }));
+      }
+    });
+  }, [dispatech, id]);
+
+  if (loading) {
+    return <LoadingPatient />;
+  }
+
+  if (error) {
+    return (
+      <div className="mx-4  text-center h-1/2 items-center justify-center   flex flex-col gap-5  ">
+        <h2 className="font-bold text-red-500 text-3xl">
+          {error.response.error}
+        </h2>
+        <Link
+          to={"/patient/Pre-eclampsia"}
+          className="bg-fuchsia-900 text-lg px-3 text-white rounded-lg py-2"
+        >
+          Search With Code
+        </Link>
+      </div>
+    );
+  }
+
+  if (preEclampsia) {
+    return (
+      <>
+        <div className="mx-4 lg:mx-16  grid  grid-cols-1 md:grid-cols-2  gap-5 ">
+          <ArticalInfo
+            description={
+              preEclampsia["history_of_pre-eclampsia"] ? "yes" : "no"
+            }
+            title={"history of pre-eclampsia"}
+          />
+
+          <ArticalInfo
+            description={preEclampsia.number_of_pregnancies_with_pe}
+            title={"number of pregnancies with pe"}
+          />
+
+          <ArticalInfo
+            description={preEclampsia.date_of_pregnancies_with_pe}
+            title={"date of pregnancies with pe"}
+          />
+
+          <ArticalInfo
+            description={preEclampsia.fate_of_the_pregnancy}
+            title={"fate of the pregnancy"}
+          />
+        </div>
+        <div className="flex print:hidden gap-x-8 gap-y-4 justify-end md:flex-row flex-col my-10  items-end md:items-center me-16">
+          <Link
+            to={`/patient/Pre-eclampsia/add/`}
+            className="rounded-lg text-white bg-blue-700 flex gap-4 px-10 py-2"
+          >
+            <MdAdd />
+            Add
+          </Link>
+          <Link
+            to={`/patient/Pre-eclampsia/update/${preEclampsia.patient_id}`}
+            className="rounded-lg text-white bg-blue-700 flex gap-4 px-10 py-2"
+          >
+            <MdEdit />
+            Edit
+          </Link>
+          <button
+            className="rounded-lg text-white bg-fuchsia-900 flex gap-4 px-10 py-2"
+            onClick={() => {
+              window.print();
+            }}
+          >
+            <FiDownload />
+            Download a copy
+          </button>
+        </div>
+      </>
+    );
+  }
 }
+
+const ArticalInfo = ({ title, description, col, phone = null }) => {
+  return (
+    <article
+      className={`flex flex-col font-medium capitalize ${
+        col && `md:col-span-${col}`
+      }`}
+    >
+      <h3 className=" text-black   text-xl">{title || ""}</h3>
+      {phone ? (
+        <a
+          className=" text-gray-600 text-base w-fit"
+          href={`tel:${description}`}
+        >
+          {description || ""}
+        </a>
+      ) : (
+        <p className=" text-gray-600 text-base">{description || ""}</p>
+      )}
+    </article>
+  );
+};
