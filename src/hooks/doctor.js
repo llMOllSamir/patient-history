1f18c9bd43705603778bc89a18a5cf4ef71489e2
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2024-04-23 22:30:31 +0200

    Add delete logic (#19)

--- src/hooks/doctor.js
+++ src/hooks/doctor.js
@@ -1,20 +1,32 @@
-import { useQueries } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import fetching from "../fetchingRequest";
 
-export const useGetDoctorsAndAdminActivities = ({ id }) => {
+export const useGetDoctors = ({ doctorsOnSuccess, doctorsOnError }) => {
   const fetchDoctors = () => fetching().get(`/doctors`);
+
+  return useQuery({
+    queryKey: "doctors",
+    queryFn: fetchDoctors,
+    onSuccess: doctorsOnSuccess,
+    onError: doctorsOnError,
+  });
+};
+
+export const useGetAdminActivities = ({
+  id,
+  adminActivitiesOnSuccess,
+  adminActivitiesOnError,
+}) => {
   const fetchAdminActivities = () => fetching().get(`/admins/${id}/history`);
+  return useQuery({
+    queryKey: "adminActivity",
+    queryFn: fetchAdminActivities,
+    onSuccess: adminActivitiesOnSuccess,
+    onError: adminActivitiesOnError,
+  });
+};
 
-  const queries = [
-    {
-      queryKey: "doctors",
-      queryFn: fetchDoctors,
-    },
-    {
-      queryKey: "adminActivities",
-      queryFn: fetchAdminActivities,
-    },
-  ];
-
-  return useQueries(queries);
+const deleteDoctor = (id) => fetching().delete(`/doctors/${id}`);
+export const useDeleteDoctor = ({ onSuccess }) => {
+  return useMutation(deleteDoctor, { onSuccess });
 };
