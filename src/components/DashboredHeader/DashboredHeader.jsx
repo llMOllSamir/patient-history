49df308b128b2424e8ad0c4fe1fc99a4ac128cfe
Mohamed Samir <121590212+llMOllSamir@users.noreply.github.com>
2024-05-17 22:26:04 +0300

    Mohamed (#25)
    * fix baseurl
    
    * New URL

--- src/components/DashboredHeader/DashboredHeader.jsx
+++ src/components/DashboredHeader/DashboredHeader.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from "./DashboredHeader.module.css";
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../store/slices/authSlice';
 import { useLogout } from '../../hooks/auth';
-
+import logo from "../../assets/images/logo-transparent.svg"
 export default function DashboredHeader() {
   const navigate = useNavigate();
   const dispatch = useDispatch()
@@ -16,9 +16,13 @@ export default function DashboredHeader() {
   const { refetch } = useLogout({ onSuccess })
   return (
     <>
-      {user && <header className='w-full print:hidden grow relative z-50 bg-fuchsia-900 text-white  h-20 mb-10 flex justify-end items-center px-10 gap-5  '>
-        <h2 className=' capitalize text-lg select-none decoration-2 underline underline-offset-4  font-bold'>Dr.{user?.name.split(" ").slice(0, 2).join(" ") || ""}</h2>
-        <button onClick={refetch} className='border border-white px-8 rounded-xl py-1'>Log Out</button>
-      </header>}</>
+      {user &&
+        <header className='w-full print:hidden grow relative z-50 bg-fuchsia-900 text-white  h-24 mb-10 flex justify-between items-center px-10 gap-5'>
+          <Link to={"/"}> <img src={logo} alt='logo' className='w-20 aspect-square' /></Link>
+          <div className='flex  gap-5'>
+            <h2 className=' capitalize text-lg select-none decoration-2 underline underline-offset-4  font-bold'>Dr.{user?.name.split(" ").slice(0, 2).join(" ") || ""}</h2>
+            <button onClick={refetch} className='border border-white px-8 rounded-xl py-1'>Log Out</button>
+          </div>
+        </header>}</>
   )
 }
--- src/fetchingRequest.js
+++ src/fetchingRequest.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const fetching = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "https://f41e-156-208-210-223.ngrok-free.app/api",
+    baseURL:
+      "https://patients-management-system-production-6391.up.railway.app/api",
     headers: {
       Authorization: `Bearer ${token}`,
     },
